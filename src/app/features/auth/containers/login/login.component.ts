2a1b86f57a6c00a4efc8b5ed025b9c8fe68e93d9
issmail.basel <issmail.basel@gmail.com>
2023-08-22 19:37:45 +0300

    Add login component

---
+++ src/app/features/auth/containers/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'ecs-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  isPassVisible = false;
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    remember: [''],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  get loginFormControl() {
+    return this.loginForm.controls;
+  }
+
+  hasEmailError(errorType: string): boolean {
+    return this.loginFormControl.email.hasError(errorType);
+  }
+
+  hasPasswordError(errorType: string): boolean {
+    return this.loginFormControl.password.hasError(errorType);
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // TODO: implement
+      console.log('Form Submitted Successfully');
+    }
+  }
+}
