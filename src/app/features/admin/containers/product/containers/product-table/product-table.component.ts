f6602627f2b23a5cf4c24974b978ea268fd88b8f
issmail.basel <issmail.basel@gmail.com>
2023-08-23 16:11:46 +0300

    Add products table

--- src/app/features/admin/containers/product/containers/product-table/product-table.component.ts
+++ src/app/features/admin/containers/product/containers/product-table/product-table.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Observable } from 'rxjs';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'ecs-product-table',
   templateUrl: './product-table.component.html',
   styleUrls: ['./product-table.component.scss'],
 })
-export class ProductTableComponent {}
+export class ProductTableComponent {
+  products$: Observable<Product[]> = this.productService.getAllProducts();
+
+  displayedColumns: string[] = [
+    'title',
+    'price',
+    'description',
+    'category',
+    'image',
+  ];
+  data = [];
+  columns = {
+    cols: ['title'],
+  };
+
+  constructor(private productService: ProductService) {}
+}
--- src/app/features/admin/containers/product/product.module.ts
+++ src/app/features/admin/containers/product/product.module.ts
@@ -3,9 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductTableComponent } from './containers/product-table/product-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [ProductTableComponent],
-  imports: [CommonModule, ProductRoutingModule],
+  imports: [
+    CommonModule,
+    ProductRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+  ],
 })
 export class ProductModule {}
