75e5d33e5320f53f68d2fc8475d32e1ee6ca9814
issmail.basel <issmail.basel@gmail.com>
2023-08-24 01:35:27 +0300

    Add role guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
+import { preventLoginGuard } from './core/guards/prevent-login.guard';
 
 const routes: Routes = [
   {
     path: 'homepage',
     canActivate: [authGuard],
+    data: { role: 'user' },
     loadChildren: () =>
       import('./features/user/user.module').then(m => m.UserModule),
   },
   {
     path: 'admin',
+    data: { role: 'admin' },
     canActivate: [authGuard],
     loadChildren: () =>
       import('./features/admin/admin.module').then(m => m.AdminModule),
   },
   {
     path: 'auth',
+    canActivate: [preventLoginGuard],
     loadChildren: () =>
       import('./features/auth/auth.module').then(m => m.AuthModule),
   },
