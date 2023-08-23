dd0eb1a847ab2769509993d3b2afafe2b34e6cc8
issmail.basel <issmail.basel@gmail.com>
2023-08-23 21:34:19 +0300

    Add filter

--- src/app/features/user/components/categories-section/category-card/category-card.component.ts
+++ src/app/features/user/components/categories-section/category-card/category-card.component.ts
@@ -13,17 +13,27 @@ type CategoryColors = {
 })
 export class CategoryCardComponent implements OnInit {
   @Input() category!: string;
-  selectedCategory!: number | null;
+  selectedCategory!: string;
+  shadow = '0px 3px 15px rgba(0,0,0,0.2)';
   color: CategoryColors = {
     electronics: '#4682B4',
     jewelery: '#B76E79',
     "men's clothing": '#228B22',
-    "women's clothing": '#E6E6FA',
+    "women's clothing": '#9370DB',
   };
 
   constructor(private route: ActivatedRoute) {}
 
+  toggleColoredShadow($event: MouseEvent | FocusEvent, category: string) {
+    this.shadow =
+      $event.type == 'mouseover' || $event.type == 'focus'
+        ? '0px 3px 15px ' + this.color[category]
+        : '0px 3px 15px rgba(0,0,0,0.2)';
+  }
+
   ngOnInit(): void {
-    this.route.queryParams.pipe(map(value => value));
+    this.route.queryParams.pipe(map(value => value)).subscribe(values => {
+      this.selectedCategory = values['category'] ? values['category'] : null;
+    });
   }
 }
