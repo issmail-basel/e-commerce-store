03424f298c74bdb96180459cced72f937e9e465a
issmail.basel <issmail.basel@gmail.com>
2023-08-24 14:45:32 +0300

    Change env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   baseURL: 'https://fakestoreapi.com/',
-  arabicWebsiteBaseURL: 'https://ecs.dlclients.xyz/ar/', //TODO: change
-  englishWebsiteBaseURL: 'https://ecs.dlclients.xyz/', //TODO: change
+  arabicWebsiteBaseURL: 'https://ecs.dlclients.xyz/ar', //TODO: change
+  englishWebsiteBaseURL: 'https://ecs.dlclients.xyz', //TODO: change
 };
