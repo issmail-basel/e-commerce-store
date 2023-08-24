e8e91a804ed9e941a5a8a66b4b59a162b3b0df4e
issmail.basel <issmail.basel@gmail.com>
2023-08-24 10:18:37 +0300

    Add update/create

--- src/app/features/admin/containers/product/product.module.ts
+++ src/app/features/admin/containers/product/product.module.ts
@@ -6,15 +6,25 @@ import { ProductTableComponent } from './containers/product-table/product-table.
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatButtonModule } from '@angular/material/button';
+import { ProductFormComponent } from './containers/product-form/product-form.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [ProductTableComponent],
+  declarations: [ProductTableComponent, ProductFormComponent],
   imports: [
     CommonModule,
     ProductRoutingModule,
     MatTableModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatPaginatorModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
   ],
 })
 export class ProductModule {}
