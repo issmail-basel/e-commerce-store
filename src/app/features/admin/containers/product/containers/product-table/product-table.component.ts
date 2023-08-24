88d103dcdd6097aaae5e014ccc10223a7c6bb988
issmail.basel <issmail.basel@gmail.com>
2023-08-24 18:31:48 +0300

    Add delete

--- src/app/features/admin/containers/product/containers/product-table/product-table.component.ts
+++ src/app/features/admin/containers/product/containers/product-table/product-table.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../../models/product';
 import { ProductEntityService } from '../../services/product-entity.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'ecs-product-table',
@@ -17,14 +18,29 @@ export class ProductTableComponent implements OnInit {
     'description',
     'category',
     'image',
+    'action',
   ];
   data = [];
   columns = {
     cols: ['title'],
   };
 
-  constructor(private productService: ProductEntityService) {}
+  constructor(
+    private productService: ProductEntityService,
+    private _snackBar: MatSnackBar
+  ) {}
   ngOnInit(): void {
     this.products$ = this.productService.entities$;
+  }
+
+  deleteProduct(product: Product) {
+    this.productService.delete(product).subscribe({
+      next: () => {
+        this._snackBar.open('Deleted successfully', 'close');
+      },
+      error: (error: Error) => {
+        this._snackBar.open(error.message, 'close');
+      },
+    });
   }
 }
