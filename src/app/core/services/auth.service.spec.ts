2bb3ce83ac4177e7c6dec11833c5d1fb174e029e
Basel Issmail <78056580+issmail-basel@users.noreply.github.com>
2023-08-24 11:39:22 +0300

    Add unit tests (#62)

--- src/app/core/services/auth.service.spec.ts
+++ src/app/core/services/auth.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AuthService } from './auth.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { User } from '../models/User';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AuthService],
+    });
     service = TestBed.inject(AuthService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch user data', () => {
+    const mockUserData: User & { password: string } = {
+      token: 'bearer userToken',
+      password: 'user',
+      role: 'user',
+      username: 'user',
+    };
+
+    service.login('user', 'user').subscribe((data: User) => {
+      expect(data).toEqual(mockUserData);
+    });
   });
 });
