2dc5ffeee1fb35762be880b39cbe0446c03d8538
issmail.basel <issmail.basel@gmail.com>
2023-08-24 11:38:24 +0300

    Add unit tests

--- src/app/core/interceptors/api.interceptor.spec.ts
+++ src/app/core/interceptors/api.interceptor.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 import { ApiInterceptor } from './api.interceptor';
 
 describe('ApiInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      ApiInterceptor
-      ]
-  }));
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
-  it('should be created', () => {
-    const interceptor: ApiInterceptor = TestBed.inject(ApiInterceptor);
-    expect(interceptor).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true },
+      ],
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  it('should prepend baseURL to the request URL', () => {
+    const testUrl = 'test-endpoint';
+    httpClient.get(testUrl).subscribe();
+
+    const req = httpTestingController.expectOne(request =>
+      request.url.includes('https://fakestoreapi.com/' + testUrl)
+    );
+    expect(req.request.method).toEqual('GET');
+    req.flush({});
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 });
--- src/app/features/user/components/search-field/search-field.component.spec.ts
+++ src/app/features/user/components/search-field/search-field.component.spec.ts
@@ -1,15 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 import { SearchFieldComponent } from './search-field.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SearchFieldComponent', () => {
   let component: SearchFieldComponent;
   let fixture: ComponentFixture<SearchFieldComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SearchFieldComponent],
+      imports: [
+        RouterTestingModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatMenuModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+      ],
+    }).compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [SearchFieldComponent]
-    });
     fixture = TestBed.createComponent(SearchFieldComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,4 +41,23 @@ describe('SearchFieldComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should update search query when input changes', fakeAsync(() => {
+    const inputElement = fixture.debugElement.query(
+      By.css('input')
+    ).nativeElement;
+
+    const navigateSpy = spyOn(component['router'], 'navigate');
+
+    inputElement.value = 'test';
+    inputElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    tick(300);
+
+    expect(navigateSpy).toHaveBeenCalledWith(['/homepage'], {
+      queryParams: { search: 'test' },
+      queryParamsHandling: 'merge',
+    });
+  }));
 });
